379d9679ee148c4ccdde68552b3b6df7fb2bb93b
Alice Yang <yangalice2008@gmail.com>
2014-03-12 18:40:43 -0700

    Finished initializing RiverNetworkNode

--- RiverNetworkNode/RiverNetworkNode.cpp
+++ RiverNetworkNode/RiverNetworkNode.cpp
@@ -23,6 +23,7 @@
 #include <maya/MFnNurbsCurveData.h>
 #include <maya/MIOStream.h>
 #include <maya/MGlobal.h>
+#include <maya/MFnArrayAttrsData.h>
 
 #define McheckErr(stat,msg)			\
 	if ( MS::kSuccess != stat ) {	\
@@ -50,8 +51,6 @@
 	CHECK_MSTATUS(attr.setHidden(true));
 
 
-
-
 MStatus returnStatus;
 
 // This initializes the node’s identifier to a unique tag 
@@ -62,26 +61,71 @@ MObject     RiverNetworkNode::inputCurve;
 MObject     RiverNetworkNode::outputPoints;       
 
 
+void* RiverNetworkNode::creator()
+//
 // The creator() method simply returns new instances of this node. The return type is a void* so
 // Maya can create node instances internally in a general fashion without having to know the return type.
-void* RiverNetworkNode::creator()
+//
 {
 	return new RiverNetworkNode();
 }
 
-MStatus RiverNetworkNode::compute( const MPlug& plug, MDataBlock& data )
+
+
+MStatus RiverNetworkNode::initialize()
 //
 //	Description:
-//		This method computes the value of the given output plug based
-//		on the values of the input attributes.
+//		This method is called to create and initialize all of the attributes
+//      and attribute dependencies for this node type.  This is only called 
+//		once when the node type is registered with Maya.
 //
-//	Arguments:
-//		plug - the plug to compute
-//		data - object that provides access to the attributes for this node
-//
+//	Return Values:
+//		MS::kSuccess
+//		MS::kFailure
+//		
+{
+	MFnTypedAttribute tAttr; // for nurbsCurve, outputPoints
+	MStatus returnStatus;
+
+	// Create attributes
+	inputCurve = tAttr.create( "inputCurve", "in", MFnNurbsCurveData::kNurbsCurve, &returnStatus);
+	McheckErr(returnStatus, "ERROR creating RiverNetworkNode inputCurve attribute\n");
+	MAKE_INPUT(tAttr);
+
+	outputPoints = tAttr.create( "outputPoints", "op", MFnArrayAttrsData::kDynArrayAttrs, &returnStatus);
+	McheckErr(returnStatus, "ERROR creating RiverNetworkNode outputPoints attribute\n");
+	MAKE_OUTPUT(tAttr);
+
+	// Add attributes
+	returnStatus = addAttribute( inputCurve );
+	McheckErr(returnStatus, "ERROR adding inputCurve attribute\n");
+
+	returnStatus = addAttribute( outputPoints );
+	McheckErr(returnStatus, "ERROR adding outputPoints attribute\n");
+
+	// Set up a dependency between the input and the output.  This will cause
+	// the output to be marked dirty when the input changes.  The output will
+	// then be recomputed the next time the value of the output is requested.
+	// 
+	returnStatus = attributeAffects(inputCurve, outputPoints);
+	McheckErr(returnStatus, "ERROR in attributeAffects\n");
+
+	return MS::kSuccess;
+}
+
+MStatus RiverNetworkNode::compute( const MPlug& plug, MDataBlock& data )
+	//
+	//	Description:
+	//		This method computes the value of the given output plug based
+	//		on the values of the input attributes.
+	//
+	//	Arguments:
+	//		plug - the plug to compute
+	//		data - object that provides access to the attributes for this node
+	//
 {
 	MStatus returnStatus;
- 
+
 	// Check which output attribute we have been asked to compute.  If this 
 	// node doesn't know how to compute it, we must return 
 	// MS::kUnknownParameter.
@@ -123,54 +167,3 @@ MStatus RiverNetworkNode::compute( const MPlug& plug, MDataBlock& data )
 
 	return MS::kSuccess;
 }
-
-
-
-MStatus RiverNetworkNode::initialize()
-//
-//	Description:
-//		This method is called to create and initialize all of the attributes
-//      and attribute dependencies for this node type.  This is only called 
-//		once when the node type is registered with Maya.
-//
-//	Return Values:
-//		MS::kSuccess
-//		MS::kFailure
-//		
-{
-	// This sample creates a single input float attribute and a single
-	// output float attribute.
-	//
-	MFnNumericAttribute nAttr;
-	MStatus				stat;
-
-	inputCurve = nAttr.create( "input", "in", MFnNumericData::kFloat, 0.0 );
-	// Attribute will be written to files when this type of node is stored
- 	nAttr.setStorable(true);
-	// Attribute is keyable and will show up in the channel box
- 	nAttr.setKeyable(true);
-
-	outputPoints = nAttr.create( "output", "out", MFnNumericData::kFloat, 0.0 );
-	// Attribute is read-only because it is an output attribute
-	nAttr.setWritable(false);
-	// Attribute will not be written to files when this type of node is stored
-	nAttr.setStorable(false);
-
-	// Add the attributes we have created to the node
-	//
-	stat = addAttribute( inputCurve );
-		if (!stat) { stat.perror("addAttribute"); return stat;}
-	stat = addAttribute( outputPoints );
-		if (!stat) { stat.perror("addAttribute"); return stat;}
-
-	// Set up a dependency between the input and the output.  This will cause
-	// the output to be marked dirty when the input changes.  The output will
-	// then be recomputed the next time the value of the output is requested.
-	//
-	stat = attributeAffects( inputCurve, outputPoints );
-		if (!stat) { stat.perror("attributeAffects"); return stat;}
-
-	return MS::kSuccess;
-
-}
-
