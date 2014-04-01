60e9567f1da5449aef1a02ab5ced8afb8907145f
Alice Yang <yangalice2008@gmail.com>
2014-04-01 19:33:20 -0400

    Added a static function that increments an id value every time a new RiverNode is created and assigns it as myId. Changed == operator to check against myId values.

--- HydroTerrain/RiverNode.cpp
+++ HydroTerrain/RiverNode.cpp
@@ -1,34 +1,22 @@
 #include "RiverNode.h"
 
-#define DEFAULT_PRIORITY_INDEX 4
+#define DEFAULT_PRIORITY_INDEX 3
 #define DEFAULT_EDGE_LENGTH 5
 
-RiverNode::RiverNode()
+int RiverNode::id = 0;
+
+RiverNode::RiverNode() : myId(getId())
 {
 	pi = DEFAULT_PRIORITY_INDEX;
 	position = vec3(0,0,0);
-	e = DEFAULT_EDGE_LENGTH;
+	edgeLength = DEFAULT_EDGE_LENGTH;
 }
 
-RiverNode::RiverNode(int s)
-{
-	position = vec3(0,0,0);
-	pi = s;
-	e = DEFAULT_EDGE_LENGTH;
-}
-
-RiverNode::RiverNode(vec3 p)
+RiverNode::RiverNode(vec3 p) : myId(getId())
 {
 	position = p;
 	pi = DEFAULT_PRIORITY_INDEX;
-	e = DEFAULT_EDGE_LENGTH;
-}
-
-RiverNode::RiverNode(vec3 p, int s)
-{
-	position = p;
-	pi = s;
-	e = DEFAULT_EDGE_LENGTH;
+	edgeLength = DEFAULT_EDGE_LENGTH;
 }
 
 vec3 RiverNode::getPosition() const
@@ -44,4 +32,9 @@ bool RiverNode::compare_node_elevations(const RiverNode& x, const RiverNode& y)
 bool RiverNode::compare_node_priority_indices(const RiverNode& x, const RiverNode& y)
 {
 	return x.pi < y.pi;
+}
+
+bool operator == (RiverNode x, RiverNode y)
+{
+	return x.myId == y.myId;
 }
--- HydroTerrain/RiverNode.h
+++ HydroTerrain/RiverNode.h
@@ -12,37 +12,47 @@ enum RIVER_TYPE_T {
 	C
 };
 
+// Node types used in expansion rules
+enum NODE_TYPE_T {
+	CANDIDATE, // Candidate node (alpha)
+	INSTANTIATED, // An instantiated node (beta)
+	TERMINAL // A node that has been added to the graph and cannot be further extended (tao)
+};
+
 // Contain class that holds all necessary info about a River Node
 class RiverNode
 {
 public:
 	RiverNode();
-	RiverNode(int s);
 	RiverNode(vec3 p);
-	RiverNode(vec3 p, int s);
 	~RiverNode(){};
 
 	vec3 getPosition() const;
 	static bool compare_node_elevations(const RiverNode& x, const RiverNode& y);
 	static bool compare_node_priority_indices(const RiverNode& x, const RiverNode& y);
+	friend bool operator== ( RiverNode x, RiverNode y);
+
+	static int getId() {return id++;}
 
 public:
-	int id;
 	vec3 position;
 	bool isCandidate;
-	float e; // edge length (distance to parent)
-	std::string name;
+	float edgeLength; // distance to parent
 	int pi; // priority index (Horton-Strahler number)
+	int myId;
 	
 	RIVER_TYPE_T river_type;
-	// phi: flow
+	NODE_TYPE_T node_type;
+	
+private:
+	static int id;
 };
 
 
 // Override stream operator
 inline std::ostream& operator<<(std::ostream &strm, const RiverNode &r)
 {
-	return strm << "RiverNode:" << r.getPosition();
+	return strm << "RiverNode id:" << r.myId << " position: " << r.getPosition();
 }
 
 #endif
